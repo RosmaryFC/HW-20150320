0a3a7191265073826de9537870aaa96b0e930491
RosmaryFC <Rosmaryfc@hotmail.com>
2015-03-21 02:50:05 -0400

    completed version of APrettyTitle

--- src/nyc/c4q/RosmaryFC/APrettyTitle.java
+++ src/nyc/c4q/RosmaryFC/APrettyTitle.java
@@ -18,34 +18,44 @@ package nyc.c4q.RosmaryFC;
 public class APrettyTitle {
     public static void main(String[] args) {
 
-        //System.out.println(printTitle("a tale of two cities", '*'));
-        //System.out.println(repeat("Hello, World!",'x', 5));
-        //System.out.println(underline("Hello, World!", '*'));
+        System.out.println(printTitle("a tale of two cities", '*'));
 
-        //underline("Hello, World!", '*');
-
-        printTitle("A tale of two cities", '*');
     }
 
     public static String printTitle(String text, char underline) {
-        String text = "";
+        String newText = "";
 
-        System.out.println(text);
+        //allows code  to check through string as individual characters
+        for(int i = 0; i < text.length(); i++) {
+            if( i == 0) {//first character is always capital
+                newText += Character.toUpperCase(text.charAt(i));
+            } else
+            {
+                if (text.charAt(i -1) == ' ') { //if character is after a space it will be capital
+                    newText += Character.toUpperCase(text.charAt(i));
+                } else {// if character is not after a space it will be lowercase
+                    newText += text.charAt(i);
+                }
+            }
+        }
 
-        underline(text, underline); //calling underline method
+        System.out.println(newText);// prints parameter String text
 
-        return "";
+        String printTitle = underline(text,underline); //calling underline method
+
+        return printTitle;
     }
+
 
     //Start of code that prints out underline
-    public static void underline(String text,char letter ){
-        System.out.println(text);
+    public static String underline(String text,char letter ){ // creates the underline effect under text.
+        //System.out.println(text);
 
+        String underline = repeat(text,letter,text.length()); // calling repeat method
 
-        System.out.println(repeat(text,letter,text.length()));
-        //return "";
+        return underline;
     }
-    public static String repeat(String text, char letter , int num) {
+    public static String repeat(String text, char letter , int num) { // allows the stars to repeat for a certain length, and print space if the character above it is a space
         String repeatInfo = "";
 
         for(int i = 0; i < num; i++){
